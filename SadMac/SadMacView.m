40430dd5b0ec0b102c5c5014d7e9073adc0a3a77
Stefan Arentz <stefan@arentz.ca>
2018-09-29 09:21:13 -0400

    Update the version number in the preview

--- SadMac/SadMacView.m
+++ SadMac/SadMacView.m
@@ -30,7 +30,7 @@
             // This is a bit hacky but it works ...
             NSTextField *textField = [[NSTextField alloc] initWithFrame: NSMakeRect(0, (self.bounds.size.height-70)/2, self.bounds.size.width, 70)];
             [textField setTextColor: NSColor.whiteColor];
-            [textField setStringValue:@"Sad Mac Screen Saver 1.0\nStefan Arentz, September 2018\ngithub.com/st3fan/SadMacScreenSaver"];
+            [textField setStringValue:@"Sad Mac Screen Saver 1.1\nStefan Arentz, September 2018\ngithub.com/st3fan/SadMacScreenSaver"];
             [textField setBezeled:NO];
             [textField setDrawsBackground:NO];
             [textField setEditable:NO];
